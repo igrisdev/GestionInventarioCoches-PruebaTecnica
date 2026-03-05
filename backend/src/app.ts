9f2b061af161eebacc7a431d9668a08fcc4c2590
igrisdev <johanalvarezz237@gmail.com>
2026-03-05 09:04:33 -0500

    refactor: separación de lógica de express con el arranque

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+
 const app = express();
-const port = 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
