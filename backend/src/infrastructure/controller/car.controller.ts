5663e97e6256a94d1e98e53260782cede1a22364
igrisdev <johanalvarezz237@gmail.com>
2026-03-14 11:12:15 -0500

    feat: controladores de Car  (getAll, getById) utilizando el middleware de errores

---
+++ backend/src/infrastructure/controller/car.controller.ts
@@ -0,0 +1,55 @@
+import type { NextFunction, Request, Response } from "express";
+import { getAllCar } from "../../application/car/getAllCar.usecase";
+import { getByIdCar } from "../../application/car/geByIdCar.usecase";
+
+// obtener todos los coches de la base de datos
+export const getAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const allCars = await getAllCar();
+
+    res.status(200).json({
+      success: true,
+      data: allCars,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// obtener un coche por su id
+export const getById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { id } = req.params as { id: string };
+
+    if (!id) {
+      const error: any = new Error("No se especificó el id del coche");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const isNumberId = Number.isInteger(Number(id));
+
+    if (!isNumberId) {
+      const error: any = new Error("El id del coche no es un número");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const car = await getByIdCar({ id: Number(id) });
+
+    res.status(200).json({
+      success: true,
+      data: car,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
