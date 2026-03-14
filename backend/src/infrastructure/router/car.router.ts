90eb645018b861242b112c0f3d95994792e9643e
igrisdev <johanalvarezz237@gmail.com>
2026-03-14 12:33:00 -0500

    feat: añadida la ruta de eliminar car

--- backend/src/infrastructure/router/car.router.ts
+++ backend/src/infrastructure/router/car.router.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
-import { getAll, getById, update } from "../controller/car.controller";
+import {
+  deleteById,
+  getAll,
+  getById,
+  update,
+} from "../controller/car.controller";
 
 export const routerCar = Router();
 
 routerCar.get("/", getAll);
 routerCar.get("/:id", getById);
 routerCar.put("/:id", update);
+routerCar.delete("/:id", deleteById);
