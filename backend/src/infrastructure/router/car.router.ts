77929ed31f64dd69bff446a2b3e994205f430110
igrisdev <johanalvarezz237@gmail.com>
2026-03-14 18:50:59 -0500

    feat: comentarios a rutas, y añadida la ruta create

--- backend/src/infrastructure/router/car.router.ts
+++ backend/src/infrastructure/router/car.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import {
+  create,
   deleteById,
   getAll,
   getById,
@@ -9,7 +10,13 @@ import {
 
 export const routerCar = Router();
 
+// endpoints para obtener todos los datos de los coches
 routerCar.get("/", getAll);
+// endpoints para obtener datos de un coche
 routerCar.get("/:id", getById);
+// endpoints para crear un coche
+routerCar.post("/create", create);
+// endpoints para actualizar datos de un coche
 routerCar.put("/:id", update);
+// endpoints para eliminar un coche
 routerCar.delete("/:id", deleteById);
