12bb1ee577c5d6d23cf2d9f219cd6737e30b8a98
igrisdev <johanalvarezz237@gmail.com>
2026-03-14 12:32:19 -0500

    feat: controlador de eliminar car

--- backend/src/infrastructure/controller/car.controller.ts
+++ backend/src/infrastructure/controller/car.controller.ts
@@ -3,6 +3,7 @@ import { getAllCar } from "../../application/car/getAllCar.usecase";
 import { getByIdCar } from "../../application/car/geByIdCar.usecase";
 import type { ICar } from "../../core/entity/car.entity";
 import { updateCar } from "../../application/car/updateCar.usecase";
+import { deleteCar } from "../../application/car/deleteCar.usecase";
 
 // obtener todos los coches de la base de datos
 export const getAll = async (
@@ -82,6 +83,39 @@ export const update = async (
     const { data } = req.body as { data: Omit<ICar, "id"> };
 
     const car = await updateCar({ id: Number(id), data });
+
+    res.status(200).json({
+      success: true,
+      data: car,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { id } = req.params as { id: string };
+
+    if (!id) {
+      const error: any = new Error("No se especificó el id del coche");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const isNumberId = Number.isInteger(Number(id));
+
+    if (!isNumberId) {
+      const error: any = new Error("El id del coche no es un número");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const car = await deleteCar({ id: Number(id) });
 
     res.status(200).json({
       success: true,
