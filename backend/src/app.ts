91339b1ca408c1eab7158f0c75d39aeebb7523a2
igrisdev <johanalvarezz237@gmail.com>
2026-03-16 21:18:10 -0500

    feat: agregada la ruta de brands

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import swaggerIU from "swagger-ui-express";
+
 import { errorHandler } from "./infrastructure/middleware/errorHandle.middleware";
 import { routerCar } from "./infrastructure/router/car.router";
+import specs from "../swagger/swagger";
+import { routerBrand } from "./infrastructure/router/brand.router";
 
 const app = express();
 
@@ -10,9 +14,12 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-// Rutas
+// Ruta documentación de la API
+app.use("/api-docs", swaggerIU.serve, swaggerIU.setup(specs));
+
+// Ruta de CRUD de carros, marcas
 app.use("/api/car", routerCar);
-// app.use("/api/brand", brandRouter);
+app.use("/api/brand", routerBrand);
 
 // middleware para manejar errores de toda la aplicación
 app.use(errorHandler);
