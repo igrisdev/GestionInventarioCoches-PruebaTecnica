86a0523f12c5667abe2fbddb60cccd4132486136
igrisdev <johanalvarezz237@gmail.com>
2026-03-21 13:26:06 -0500

    feat: agregamos los apagadores de prima si se usa Ctrl+C o se apaga el contenedor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,24 @@
 import app from "./app.ts";
+import { prisma } from "./infrastructure/database/prisma.client.ts";
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}/api`);
+});
+
+// Ctrl+C en la terminal (SIGINT)
+process.on("SIGINT", async () => {
+  console.log("🛑 Apagando el servidor...");
+  await prisma.$disconnect();
+  console.log("🔌 Prisma desconectado correctamente.");
+  process.exit(0);
+});
+
+// Docker apaga el contenedor (SIGTERM)
+process.on("SIGTERM", async () => {
+  console.log("🛑 Apagando el servidor...");
+  await prisma.$disconnect();
+  console.log("🔌 Prisma desconectado correctamente.");
+  process.exit(0);
 });
