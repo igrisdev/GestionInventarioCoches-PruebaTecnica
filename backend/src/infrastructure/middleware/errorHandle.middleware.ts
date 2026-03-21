cfe8ec394ad8f8374298a7c6bc39c305059d8995
igrisdev <johanalvarezz237@gmail.com>
2026-03-21 13:27:35 -0500

    refactor: mejora del middleware de errores globales de prisma

--- backend/src/infrastructure/middleware/errorHandle.middleware.ts
+++ backend/src/infrastructure/middleware/errorHandle.middleware.ts
@@ -1,4 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
+import { Prisma } from "../../../generated/prisma/client";
 
 // Middleware para manejar errores
 export const errorHandler = (
@@ -7,11 +8,25 @@ export const errorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = err.statusCode || 500;
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Error interno del servidor";
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    // P2025 = "Record not found" (Al actualizar o eliminar algo que no existe)
+    if (err.code === "P2025") {
+      statusCode = 404;
+      message = "El registro que intentas modificar o eliminar no existe.";
+    }
+    // P2002 = "Unique constraint failed" (Ej: Si intentas crear dos marcas con el mismo nombre y tu esquema lo prohíbe)
+    else if (err.code === "P2002") {
+      statusCode = 400;
+      message = "Ya existe un registro con esos datos únicos.";
+    }
+  }
 
   res.status(statusCode).json({
     success: false,
-    message: err.message || "Error interno del servidor",
+    message: message,
     stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
 };
