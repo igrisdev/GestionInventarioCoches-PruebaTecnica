95ee0cfd203858f96582095ec8dc2f94e9e235c9
igrisdev <johanalvarezz237@gmail.com>
2026-03-21 12:53:13 -0500

    feat: mejora a las funciones de get brand  y para actualizar

--- backend/src/infrastructure/repository/brand.repository.ts
+++ backend/src/infrastructure/repository/brand.repository.ts
@@ -7,7 +7,11 @@ import { prisma } from "../database/prisma.client";
 // implementación de la interfaz IBrandRepository para el CRUD de las marcas
 export const BrandRepository: IBrandRepository = {
   getAll: async function (): Promise<any> {
-    const brands = await prisma.marca.findMany();
+    const brands = await prisma.marca.findMany({
+      include: {
+        coches: true,
+      },
+    });
 
     if (!brands) {
       const error: any = new Error("No se encontraron marcas");
@@ -20,6 +24,9 @@ export const BrandRepository: IBrandRepository = {
   getById: async function (id: number): Promise<any> {
     const brand = await prisma.marca.findUnique({
       where: { id: id },
+      include: {
+        coches: true,
+      },
     });
 
     if (!brand) {
@@ -46,8 +53,6 @@ export const BrandRepository: IBrandRepository = {
       return brand;
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        console.log("👉 EL CÓDIGO DE PRISMA ES:", error.code);
-
         if (error.code === "P2018") {
           const customError: any = new Error(
             "Uno o más coches que intentas conectar no existen en la base de datos.",
@@ -61,18 +66,29 @@ export const BrandRepository: IBrandRepository = {
     }
   },
   update: async function (id: number, data: Omit<IBrand, "id">): Promise<any> {
-    const brand = await prisma.marca.update({
-      where: { id: id },
-      data: data as any,
-    });
+    try {
+      const brand = await prisma.marca.update({
+        where: { id: id },
+        data: data as any,
+        include: {
+          coches: true,
+        },
+      });
 
-    if (!brand) {
-      const error: any = new Error(`No se actualizo la marca con el id: ${id}`);
-      error.statusCode = 404;
+      return brand;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2025") {
+          const customError: any = new Error(
+            `No se encontró la marca con el id: ${id}`,
+          );
+          customError.statusCode = 404;
+          throw customError;
+        }
+      }
+
       throw error;
     }
-
-    return brand;
   },
   delete: async function (id: number): Promise<any> {
     const brand = await prisma.marca.delete({
